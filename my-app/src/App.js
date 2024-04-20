3bcbd20ce3b4e3b88b622d28a6092ff280a667cd
llienoekul <95896606+llienoekul@users.noreply.github.com>
2024-04-20 22:11:27 +0000

    list finally!!!!!!!!!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
+    <div>
       <Navbar></Navbar>
-      HOME PAGE LUKE IS DOING RIGHT NOW
+      <div style= {{maxHeight: 200, overflow: 'auto'}}>
+        <NumberList numbers={numbers}/>
+      </div>
     </div>
-
   );
 }
+
+
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li className='scrollList'>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    // Correct! Key should be specified inside the array.
+    <ListItem key={number.toString()}
+              value={number} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5,1, 2, 3, 4, 5,1, 2, 3, 4, 5,1, 2, 3, 4, 5,];
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<NumberList numbers={numbers} />);
+
 
 export default App;
